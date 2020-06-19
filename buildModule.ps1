aa9b519c8ffb6f3c2e588262d83bb8225d473819
Smalls1652 <smalls.online@icloud.com>
2020-06-18 20:48:13 -0400

    Init build script

---
+++ buildModule.ps1
@@ -0,0 +1,21 @@
+switch ((Test-Path -Path ".\build\")) {
+    $true {
+        Remove-Item -Path ".\build" -Recurse -Force
+        break
+    }
+}
+
+$buildDir = New-Item -Path ".\build" -ItemType "Directory"
+
+Push-Location -Path ".\src\graph-email\"
+
+Start-Process -FilePath "dotnet" -ArgumentList @("clean") -NoNewWindow -Wait
+Start-Sleep -Seconds 5
+Start-Process -FilePath "dotnet" -ArgumentList @("publish") -NoNewWindow -Wait
+
+Pop-Location
+
+Copy-Item -Path ".\src\PasswordExpirationEmail" -Destination ".\build\" -Recurse -Force
+
+Copy-Item -Path ".\src\graph-email\bin\Debug\netstandard2.0\publish\graph-email.dll" -Destination ".\build\PasswordExpirationEmail\"
+Copy-Item -Path ".\src\graph-email\bin\Debug\netstandard2.0\publish\Microsoft.Identity.Client.dll" -Destination ".\build\PasswordExpirationEmail\"
