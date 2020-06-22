b3ae3bf48ffe26000ac1fc008dd7ea025da5bf2b
Smalls1652 <smalls.online@icloud.com>
2020-06-22 18:55:52 -0400

    Changed build process
    - Commented out build for 'graph-email' since it's no longer needed at
      this time.

--- buildModule.ps1
+++ buildModule.ps1
@@ -7,7 +7,17 @@ switch ((Test-Path -Path ".\build\")) {
 
 $null = New-Item -Path ".\build" -ItemType "Directory"
 
+<#
 Push-Location -Path ".\src\graph-email\"
+
+Start-Process -FilePath "dotnet" -ArgumentList @("clean") -NoNewWindow -Wait
+Start-Sleep -Seconds 5
+Start-Process -FilePath "dotnet" -ArgumentList @("publish") -NoNewWindow -Wait
+
+Pop-Location
+#>
+
+Push-Location -Path ".\src\password-expiration-classes\"
 
 Start-Process -FilePath "dotnet" -ArgumentList @("clean") -NoNewWindow -Wait
 Start-Sleep -Seconds 5
@@ -17,5 +27,4 @@ Pop-Location
 
 Copy-Item -Path ".\src\PasswordExpirationEmail" -Destination ".\build\" -Recurse -Force
 
-Copy-Item -Path ".\src\graph-email\bin\Debug\netstandard2.0\publish\graph-email.dll" -Destination ".\build\PasswordExpirationEmail\"
-Copy-Item -Path ".\src\graph-email\bin\Debug\netstandard2.0\publish\Microsoft.Identity.Client.dll" -Destination ".\build\PasswordExpirationEmail\"
+Copy-Item -Path ".\src\password-expiration-classes\bin\Debug\netstandard2.0\publish\password-expiration-classes.dll" -Destination ".\build\PasswordExpirationEmail\"
