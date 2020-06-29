89ceef89f5efb7f67f6726f2eb887248b9815049
Smalls1652 <smalls.online@icloud.com>
2020-06-29 18:39:01 -0400

    Updated build script

--- buildModule.ps1
+++ buildModule.ps1
@@ -9,9 +9,15 @@ $null = New-Item -Path ".\build" -ItemType "Directory"
 
 Push-Location -Path ".\src\graph-email\"
 
-Start-Process -FilePath "dotnet" -ArgumentList @("clean") -NoNewWindow -Wait
-Start-Sleep -Seconds 5
-Start-Process -FilePath "dotnet" -ArgumentList @("publish") -NoNewWindow -Wait
+Start-Process -FilePath "dotnet" -ArgumentList @("clean") -Wait
+Start-Process -FilePath "dotnet" -ArgumentList @("publish") -Wait
+
+Pop-Location
+
+Push-Location -Path ".\src\password-expiration-classes\"
+
+Start-Process -FilePath "dotnet" -ArgumentList @("clean") -Wait
+Start-Process -FilePath "dotnet" -ArgumentList @("publish") -Wait
 
 Pop-Location
 
@@ -19,3 +25,6 @@ Copy-Item -Path ".\src\PasswordExpirationEmail" -Destination ".\build\" -Recurse
 
 Copy-Item -Path ".\src\graph-email\bin\Debug\netstandard2.0\publish\graph-email.dll" -Destination ".\build\PasswordExpirationEmail\"
 Copy-Item -Path ".\src\graph-email\bin\Debug\netstandard2.0\publish\Microsoft.Identity.Client.dll" -Destination ".\build\PasswordExpirationEmail\"
+Copy-Item -Path ".\src\graph-email\bin\Debug\netstandard2.0\publish\Newtonsoft.Json.dll" -Destination ".\build\PasswordExpirationEmail\"
+
+Copy-Item -Path ".\src\password-expiration-classes\bin\Debug\netstandard2.0\publish\password-expiration-classes.dll" -Destination ".\build\PasswordExpirationEmail\"
