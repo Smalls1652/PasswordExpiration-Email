6d409529b33ec206fb947aa2cd065e81df7d4843
Timothy Small <smalls.online@icloud.com>
2021-08-12 20:41:03 -0400

    Updated GetUsersWithExpiringPasswords
    - The entire function now uses re-usable methods in the
      'SharedFunctionMethods' static class.

--- src/PasswordExpiration.AzFunction/functions/GetUsersWithExpiringPasswords.cs
+++ src/PasswordExpiration.AzFunction/functions/GetUsersWithExpiringPasswords.cs
@@ -42,93 +42,19 @@ namespace PasswordExpiration.AzFunction
             var logger = executionContext.GetLogger("GetUsersWithExpiringPasswords");
             logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            UserSearchConfigItem userSearchConfig;
-            switch (string.IsNullOrEmpty(configId))
-            {
-                case true:
-                    userSearchConfig = functionsConfigSvc.GetUserSearchConfig("65ef0352-9545-42f8-b70f-6dd21fa8df6b");
-                    break;
-
-                default:
-                    userSearchConfig = functionsConfigSvc.GetUserSearchConfig(configId);
-                    break;
-            }
+            UserSearchConfigItem userSearchConfig = SharedFunctionMethods.GetUserSearchConfigItem(functionsConfigSvc, configId);
 
             logger.LogInformation($"User Search Config to be used: {userSearchConfig.Name} ({userSearchConfig.Id})");
 
             // Create the 'UserTools' object from the GraphClientService.
             UserTools graphUserTools = new UserTools(graphClientSvc);
 
-            /*
-                The following section is to prep for running the tasks by:
-                    - Creating a task throttler (using SemaphoreSlim) to limit the amount of concurrent threads used to 10.
-                    - Creating a 'ConcurrentBag' for the tasks to store each user data in.
-                    - Creating a 'List' for storing the created tasks in.
-            */
-            SemaphoreSlim taskThrottler = new SemaphoreSlim(10);
-            ConcurrentBag<UserPasswordExpirationDetails> usersWithExpiringPasswordsBag = new ConcurrentBag<UserPasswordExpirationDetails>();
-            List<Task> taskList = new List<Task>();
-
-            // Run a for loop to increment from 65 to 90 (The UTF32 decimal codes for the letters A-Z) to create the tasks to get users.
-            for (int i = 65; i <= 90; i++)
-            {
-                // If we reach the max number of concurrent tasks, then wait to create the rest of the tasks.
-                await taskThrottler.WaitAsync();
-
-                // Convert the current loop integer (i) value into the corresponding UTF32 character.
-                string lastNameStartsWith = Char.ConvertFromUtf32(i);
-                logger.LogInformation($"Creating task for getting users, under the '{userSearchConfig.DomainName}' domain and their last name starts with '{lastNameStartsWith}', with expiring passwords.");
-
-                // Create the task to get the users.
-                taskList.Add(
-                    Task.Run(
-                        () =>
-                        {
-                            try
-                            {
-                                // Get all of the users matching the search criteria.
-                                List<UserPasswordExpirationDetails> userPasswordExpirationDetails = ExpiringPasswordFinder.GetUsersWithExpiringPasswords(
-                                    graphUserTools,
-                                    userSearchConfig.DomainName,
-                                    userSearchConfig.OuPath,
-                                    lastNameStartsWith,
-                                    TimeSpan.FromDays(userSearchConfig.MaxPasswordAge),
-                                    TimeSpan.FromDays(10)
-                                );
-
-                                // Add each found user into the 'ConcurrentBag' of all the users.
-                                foreach (UserPasswordExpirationDetails user in userPasswordExpirationDetails)
-                                {
-                                    usersWithExpiringPasswordsBag.Add(user);
-                                }
-                            }
-                            finally
-                            {
-                                // When the task is finished, release the thread for the next task to utilize.
-                                taskThrottler.Release();
-                            }
-                        }
-                    )
-                );
-            }
-
-            // Wait for the remaining tasks to finish.
-            logger.LogInformation("Waiting for all remaining tasks to complete.");
-            Task.WaitAll(taskList.ToArray());
-
-            // Dispose of all of the tasks that were created.
-            logger.LogInformation("Cleaning up resources.");
-            foreach (Task task in taskList)
-                task.Dispose();
-
-            // Convert the 'ConcurrentBag' to a 'List'. Then clear all of the values out of the 'ConcurrentBag'.
-            List<UserPasswordExpirationDetails> usersWithExpiringPasswords = new List<UserPasswordExpirationDetails>(
-                usersWithExpiringPasswordsBag
+            List<UserPasswordExpirationDetails> usersWithExpiringPasswords = await SharedFunctionMethods.GetUserDetailsByLastNames(
+                graphUserTools,
+                userSearchConfig,
+                logger
             );
-            usersWithExpiringPasswordsBag.Clear();
 
-            // Sort all of the items alphabetically in the 'List' by each user's last name.
-            usersWithExpiringPasswords.Sort((item1, item2) => string.Compare(item1.User.Surname, item2.User.Surname));
 
             // Generate the response to send back to the client with the parsed user information.
             logger.LogInformation("Returning data to client.");
